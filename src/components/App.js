8c1ea4abfc65a890ce6e57af8fdbd12d579b121e
Elijah Smith <elijahsmith.m@gmail.com>
2022-04-10 01:04:01 -0500

    fetch and render data, loading, useEffect, useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogsInDiv, setDogsInDiv] = useState([])
+  const [isLoaded, setIsLoaded] = useState("");
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+          console.log(data.message)
+        setDogsInDiv(data.message);
+        setIsLoaded(true)
+      });
+
+    
+  }, [isLoaded]);
+
+  if (!isLoaded) return <p>Loading...</p>;
+
+  return <img src={dogsInDiv} alt="A Random Dog"></img>
+}
+
+export default App;
